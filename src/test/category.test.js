ce94130c0a790397500b6241c1ab1cbaf7eb013e
hongdiem <mingukmanse673@gmail.com>
2025-01-15 21:14:37 +0700

    category test

---
+++ src/test/category.test.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const request = require('supertest');
+const httpStatus = require('http-status');
+const { createNewCategory } = require('../controllers/category.controller');
+
+jest.mock('../services/category.service', () => ({
+    createNewCategoryService: jest.fn(),
+}));
+
+const app = express();
+app.use(express.json());
+
+app.post('/categories', createNewCategory);
+
+describe('Create new category: POST /categories', () => {
+    it('should respond with a 201 status code and return the new category data', async () => {
+        const newCategory = { name: 'Test Category' };
+        const mockResponse = { id: 1, name: 'Test Category' };
+
+        const { createNewCategoryService } = require('../services/category.service');
+        createNewCategoryService.mockResolvedValue(mockResponse);
+
+        const res = await request(app)
+            .post('/categories')
+            .send(newCategory);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toEqual({ id: 1, name: 'Test Category' });
+    });
+
+    it('should respond with a 400 status code when category name is missing', async () => {
+        const testCase = [
+            {},
+            { description: 'Test category description' }
+        ];
+
+        testCase.forEach(async (element) => {
+            const res = await request(app).post('/categories').send(element);
+            expect(res.status).toBe(400);
+        });
+    });
+
+    it('should respond with a 400 status code when category name already exists', async () => {
+        const newCategory = { name: 'Test Category' };
+        const { createNewCategoryService } = require('../services/category.service');
+    
+        jest.mock('../services/category.service');
+        createNewCategoryService.mockRejectedValue({
+            statusCode: 400,
+            message: 'This category name already taken',
+        });
+    
+        const res = await request(app).post('/categories').send(newCategory);
+    
+        expect(res.status).toBe(400);
+        expect(res.body).toEqual({});
+    });
+});
