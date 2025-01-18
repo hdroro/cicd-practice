7c38a73ec424028b43951a3e6760d820397c3055
hongdiem <mingukmanse673@gmail.com>
2025-01-18 17:36:47 +0700

    ci-cd, change db

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,11 +2,7 @@ const { createClient } = require('redis');
 require("dotenv").config();
 
 const redisClient = createClient({
-  socket: {
-    host: 'localhost',
-    port: process.env.REDIS_PORT,
-    connectTimeout: 10000,
-  },
+  url: process.env.REDIS_URL,
   retryStrategy: (times) => {
     return Math.min(times * 50, 2000);
   }
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const ApiError = require('../../../odm/src/utils/ApiError');
+const ApiError = require('../utils/ApiError');
 const httpStatus = require('http-status').status;
 require('dotenv').config();
 
