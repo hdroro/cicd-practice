f03f785f6364b08bb3ccfbeef39600ffef72cbeb
hongdiem <mingukmanse673@gmail.com>
2025-01-18 19:29:07 +0700

    update

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+console.log('process.env.DB_USER', process.env.DB_USER);
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  }
+};
+
+fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(config, null, 2));
+
+console.log('config.json has been generated!');
