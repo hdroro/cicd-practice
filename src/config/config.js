211e36781d4c8861e9d02650c8a8d3db56a84a38
hongdiem <mingukmanse673@gmail.com>
2025-01-18 22:43:06 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,32 @@
-const path = require('path');
-require('dotenv').config();
-
 const config = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'postgres',
+    dialect: 'postgres',
     port: process.env.DB_PORT,
-    migrations: {
-      path: path.resolve(__dirname, '../migrations')
-    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'postgres',
+    dialect: 'postgres',
     port: process.env.DB_PORT,
-    migrations: {
-      path: path.resolve(__dirname, '../migrations')
-    },
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
 
-module.exports = config
+module.exports = config[process.env.NODE_ENV || 'development'];
